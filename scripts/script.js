1c0c180922f9eebc5608964a18db81d46fb6073c
kabirt7 <kabir.t7@hotmail.com>
2023-12-27 12:45:12 +0800

    added module js structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-// banner
+// // banner
 
-const appleWrapper = document.querySelector("#apple-wrapper");
-const appleMenu = document.querySelector("#apple-menu");
+// const appleWrapper = document.querySelector("#apple-wrapper");
+// const appleMenu = document.querySelector("#apple-menu");
 
-appleWrapper.addEventListener("click", function () {
-  appleMenu.classList.toggle("banner__list__menu--hidden");
-  appleMenu.classList.toggle("banner__list__menu--show");
-});
+// appleWrapper.addEventListener("click", function () {
+//   appleMenu.classList.toggle("banner__list__menu--hidden");
+//   appleMenu.classList.toggle("banner__list__menu--show");
+// });
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,5 +1,36 @@
-// on click header functionalities
+// Banner Menu Toggle
+
+export const toggleMenu = () => {
+  const modifiedElement = document.querySelector("#apple-menu");
+  modifiedElement.classList.toggle("banner__list__menu--hidden");
+  modifiedElement.classList.toggle("banner__list__menu--show");
+};
+
+// Example usage:
+// setupModalToggle("#modal-wrapper", "#modal-content", "modal--hidden", "modal--show");
 
 // on click footer functionalities
 
-//
+// const appleWrapper = document.querySelector("#apple-wrapper");
+// const appleMenu = document.querySelector("#apple-menu");
+
+// appleWrapper.addEventListener("click", function () {
+//   appleMenu.classList.toggle("banner__list__menu--hidden");
+//   appleMenu.classList.toggle("banner__list__menu--show");
+// });
+
+// toggle function
+// export function displayToggle(
+//   clickedElementSelector,
+//   modifiedElementSelector,
+//   hiddenClass,
+//   showClass
+// ) {
+//   const clickedElement = document.querySelector(clickedElementSelector);
+//   const modifiedElement = document.querySelector(modifiedElementSelector);
+
+//   clickedElement.addEventListener("click", function () {
+//     modifiedElement.classList.toggle(hiddenClass);
+//     modifiedElement.classList.toggle(showClass);
+//   });
+// }
