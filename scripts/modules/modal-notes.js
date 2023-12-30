335ca4325c43d080733db3fb6fb864ccd00443d2
kabirt7 <kabir.t7@hotmail.com>
2023-12-30 15:14:19 +0800

    added desktop icons and formatting to notes app

--- scripts/modules/modal-notes.js
+++ scripts/modules/modal-notes.js
@@ -2,27 +2,26 @@ export const notesModal = () => {
   const html = document.querySelector("#htmlRef-notes");
   html.innerHTML === ""
     ? (html.innerHTML = `
-    <div class="photo-viewer notes">
-     <section class="left notes__left">
-      <ul class="left__header notes__left__header">
+    <div class="notes">
+
+     <section class="notes__banner">
+      <ul class="notes__window-menu">
        <li class="red"></li>
        <li class="yellow"></li>
        <li class="green"></li>
       </ul>
-     
-       <ul>
-        
-       </ul>
+      <button></button>
+      <button></button>
      </section> 
-     <section class="right">
-      <div class="right__header">
-       
-       <h3 class="right__header__title">My Dog</h3>
-      </div>
-      <ul class="right__icon__container">
-       
-       </div>
-      </ul>
+     <section class="notes__container">
+       <ul class="notes__list">
+        <li>Note 1</li>
+        <li>Note 2</li>
+        <li>Note 3</li>
+       </ul>
+       <main class="notes__create">
+        <h1>hello</h1>
+       </main>
      </section>
     </div>
     `)
